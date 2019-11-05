0da3d9625cd72cb46d53b2707b9a946ce13beac6
Trachsell <b_trachsell@ats-hr420-dm18.fc.ca>
2019-11-05 13:23:57 -0500

    Started moving things into functions, Still doesn't work

--- game.py
+++ game.py
@@ -1,16 +1,19 @@
 from random import randint
 import time
+from gamefunctions import winlose
+from gamefunctions import compare
 
 #Choices is an array, an array is a container that can hold multiple values
+
 choices = ["rock", "paper", "scissors"]
 computer = choices[randint (0, 2)]
-pwin = ("\033[32m            ** player wins ** \033[30;0;0m")
-cwin = ("\033[31m            ** comp wins ** \033[30;0;0m")
+pwin = ("\033[32m            ** Player wins! ** \033[30;0;0m")
+cwin = ("\033[31m            ** Comp wins! ** \033[30;0;0m")
 plives = 5
 clives = 5
 player = False
 #State variables
- 
+
 
 print ("\n\n\n\n\n\033[35m * * * * Welcome to Rock Paper Scissors in Python! * * * * \033[30;0;0m")
 # Prints before the loop so it only shows once.
@@ -20,7 +23,7 @@ print ("\n\n\n\n\n\033[35m * * * * Welcome to Rock Paper Scissors in Python! * *
 # Loops the game until someone wins
 while player == False:
 
-	print ("\n\n \033[34m ----------------------------------------------------\033[30;0;0m" )
+	print ("\n\n \033[34m ------------------------------------------------\033[30;0;0m" )
 	time.sleep(1.5)
 	# Separates each round and delays the next round for easier reading of last rounds results
 	
@@ -43,88 +46,98 @@ while player == False:
 	#displays what each player chose, if the player did not choose quit
 	
 	#proceeds to check who the winner is, and prints the result
-	if ( player == computer ):
-		print ("  You and the computer chose the same! \n")
-		print ( "\033[35m            ** Tie! **\033[30;0;0m" )
-
-	elif ( player == "rock" ):
-		if (computer == "paper"):
-			print ("           Paper covers rock! \n")
-			print (cwin)
-			plives = plives - 1
-
-		elif (computer == "scissors"):
-			print ("          Rock smashes scissors! \n")
-			print (pwin)
-			clives = clives - 1
-
-	elif ( player == "paper"):
-		if (computer == "rock"):
-			print ("           Paper covers rock! \n")
-			print (pwin)
-			clives = clives - 1
-
-		elif (computer == "scissors"):
-			print ("           Scissors cut paper! \n")
-			print (cwin)
-			plives = plives - 1
-
-
-	elif ( player == "scissors" ):
-		if ( computer == "paper"):
-			print ("           Scissors cut paper! \n")
-			print (pwin)
-			clives = clives - 1
-
-		elif (computer == "rock"):
-			print ("          Rock smashes scissors! \n")
-			print (cwin)
-			plives = plives - 1
-
-	else:
-		print ("     Sorry, that's not a valid answer! ")
-		#if an answer is not rock paper or scissors.
-
-	if (plives is 0):
-		#if either the player lives or computer lives reach 0:
-
-		print ("\n\n \033[34m ----------------------------------------------------\033[30;0;0m" )
-		print ("\n\n     You lose, You are out of lives! " )
-		print ("      Would you like to play again? \n")
-		choice = input ("                Y / N \n")
-
-		if (choice == "N" or choice == "n"):
-			print ("\n\n\n              Thank you for playing!\n\n\n")
-			exit ()
-
-		elif (choice == "Y" or choice == "y"):
-			print ("\n\n Preparing to start again... ")
-			plives = 5
-			clives = 5
-
-		else :
-			print ("E")
-			
-	elif (clives is 0):
-
-		print ("\n\n \031[34m ----------------------------------------------------\033[30;0;0m" )
-		print ("\n\n   You Win, Your opponent is out of lives!" )
-		print ("        Would you like to play again? \n")
-		choice = input ("                  Y / N \n")
-
-		if (choice == "N" or choice == "n"):
-			print ("\n\n\n              Thank you for playing!\n\n\n")
-			exit ()
-
-		elif (choice == "Y" or choice == "y"):
-			print ("\n\n Preparing to start again... ")
-			plives = 5
-			clives = 5
-
-		else:
-			print ("E")
-
 	
+
+	compare.comparing()
+
+	# if ( player == computer ):
+	# 	print ("  You and the computer chose the same! \n")
+	# 	print ( "\033[35m            ** Tie! **\033[30;0;0m" )
+
+	# elif ( player == "rock" ):
+	# 	if (computer == "paper"):
+	# 		print ("           Paper covers rock! \n")
+	# 		print (cwin)
+	# 		plives = plives - 1
+
+	# 	elif (computer == "scissors"):
+	# 		print ("          Rock smashes scissors! \n")
+	# 		print (pwin)
+	# 		clives = clives - 1
+
+	# elif ( player == "paper"):
+	# 	if (computer == "rock"):
+	# 		print ("           Paper covers rock! \n")
+	# 		print (pwin)
+	# 		clives = clives - 1
+
+	# 	elif (computer == "scissors"):
+	# 		print ("           Scissors cut paper! \n")
+	# 		print (cwin)
+	# 		plives = plives - 1
+
+
+	# elif ( player == "scissors" ):
+	# 	if ( computer == "paper"):
+	# 		print ("           Scissors cut paper! \n")
+	# 		print (pwin)
+	# 		clives = clives - 1
+
+	# 	elif (computer == "rock"):
+	# 		print ("          Rock smashes scissors! \n")
+	# 		print (cwin)
+	# 		plives = plives - 1
+
+	# else:
+	# 	print ("     Sorry, that's not a valid answer! ")
+	# 	#if an answer is not rock paper or scissors.
+		
+
+	if plives == 0:
+		winlose.winolose(" lose!")
+
+	if clives == 0:
+		winlose.winolose(" win!")	
+
+
+	# if (plives is 0):
+	# 	#if either the player lives or computer lives reach 0:
+
+	# 	print ("\n\n \033[34m ----------------------------------------------------\033[30;0;0m" )
+	# 	print ("\n\n     You lose, You are out of lives! " )
+	# 	print ("      Would you like to play again? \n")
+	# 	choice = input ("                Y / N \n")
+
+	# 	if (choice == "N" or choice == "n"):
+	# 		print ("\n\n\n              Thank you for playing!\n\n\n")
+	# 		exit ()
+
+	# 	elif (choice == "Y" or choice == "y"):
+	# 		print ("\n\n Preparing to start again... ")
+	# 		plives = 5
+	# 		clives = 5
+
+	# 	else :
+	# 		print ("E")
+			
+	# elif (clives is 0):
+
+	# 	print ("\n\n \033[34m ----------------------------------------------------\033[30;0;0m" )
+	# 	print ("\n\n   You Win, Your opponent is out of lives!" )
+	# 	print ("        Would you like to play again? \n")
+	# 	choice = input ("                  Y / N \n")
+
+	# 	if (choice == "N" or choice == "n"):
+	# 		print ("\n\n\n              Thank you for playing!\n\n\n")
+	# 		exit ()
+
+	# 	elif (choice == "Y" or choice == "y"):
+	# 		print ("\n\n Preparing to start again... ")
+	# 		plives = 5
+	# 		clives = 5
+
+	# 	else:
+	# 		print ("E")	
 	
 	player = False
 	computer = choices[randint (0, 2)]
---
+++ gamefunctions/compare.py
@@ -0,0 +1,55 @@
+import time
+from random import randint
+
+def comparing ():
+
+	global player
+	global computer
+	global plives
+	global clives
+	global pwin
+	global cwin
+
+
+	if ( player == computer ):
+		print ("  You and the computer chose the same! \n")
+		print ( "\033[35m            ** Tie! **\033[30;0;0m" )
+
+	elif ( player == "rock" ):
+		if (computer == "paper"):
+			print ("           Paper covers rock! \n")
+			print (cwin)
+			plives = plives - 1
+
+		elif (computer == "scissors"):
+			print ("          Rock smashes scissors! \n")
+			print (pwin)
+			clives = clives - 1
+
+	elif ( player == "paper"):
+		if (computer == "rock"):
+			print ("           Paper covers rock! \n")
+			print (pwin)
+			clives = clives - 1
+
+		elif (computer == "scissors"):
+			print ("           Scissors cut paper! \n")
+			print (cwin)
+			plives = plives - 1
+
+
+	elif ( player == "scissors" ):
+		if ( computer == "paper"):
+			print ("           Scissors cut paper! \n")
+			print (pwin)
+			clives = clives - 1
+
+		elif (computer == "rock"):
+			print ("          Rock smashes scissors! \n")
+			print (cwin)
+			plives = plives - 1
+
+	else:
+		print ("     Sorry, that's not a valid answer! ")
+		#if an answer is not rock paper or scissors.
+		
